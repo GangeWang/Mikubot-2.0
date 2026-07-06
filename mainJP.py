9dcf28ad01d942c454208d8a421dfce95f9771c0
gange <william20040911@gmail.com>
2026-07-06 12:22:07 +0800

    更新為使用.env

--- mainJP.py
+++ mainJP.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("DISCORD_JP_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
